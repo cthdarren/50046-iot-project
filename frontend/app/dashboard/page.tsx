449ae4188e135595d18f8b3ffdb2f4d314b59276
Darren Chan <ctze.darren@gmail.com>
2025-12-01 17:58:34 +0800

    added tanstack react table for data tables
    Signed-off-by: Darren Chan <ctze.darren@gmail.com>

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -6,8 +6,6 @@ export default function Dashboard() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <div>
-          <p>Current ID: {id ?? "None"}</p>
-          <button onClick={() => setId(123)}>Set ID to 123</button>
         </div>
     </div>
   );
