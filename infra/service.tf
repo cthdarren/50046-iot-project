a78e3e18c74d81b28705ff787808fb5510e20fcb
ashley-koh <ashleykoh24@gmail.com>
2025-12-02 21:45:38 +0800

    feat: increased replica size for availability service to 2

--- infra/service.tf
+++ infra/service.tf
@@ -72,7 +72,7 @@ resource "aws_ecs_service" "availability_service" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.availability_service.arn
   launch_type     = "FARGATE"
-  desired_count   = 1
+  desired_count   = 2
 
   network_configuration {
     subnets = [
