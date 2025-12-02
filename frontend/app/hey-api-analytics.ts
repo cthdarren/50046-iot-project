10b791a32dd700a8cee91ec308f02e9f2bb24130
Darren Chan <ctze.darren@gmail.com>
2025-12-02 13:05:29 +0800

    updated hey api routes
    Signed-off-by: Darren Chan <ctze.darren@gmail.com>

--- frontend/app/hey-api-analytics.ts
+++ frontend/app/hey-api-analytics.ts
@@ -2,5 +2,5 @@ import type { CreateClientConfig } from '@hey-api/client-axios';
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
-  baseURL: 'http://iot-alb-359304225.ap-southeast-1.elb.amazonaws.com/analytics',
+  baseURL: 'https://tingtangwalawalabingbang.com/analytics',
 });
